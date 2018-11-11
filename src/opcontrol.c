7c805f26d483577e3e3da117a12eecf3ece71856
Wesley Chalmers <wesleyjchalmers@gmail.com>
2018-11-11 11:24:53 -0500

    Arm position limiting, use of #defines for motor port mappings, and more

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -4,6 +4,15 @@
 // Team Pororo Robot Operator Control Code
 // Copyright (c) Wesley Chalmers, 2018
 
+// motor ports
+#define L_DRIVE 2
+#define R_DRIVE 3
+#define INTAKE 4
+#define PUNCHER 1
+#define ARM 10
+
+// sensor ports
+#define ARMPOT 1
 
 // arm constants
 #define ARM_MIN 3200
@@ -33,7 +42,7 @@ bool intakeButtonPressed = 0;
 
 void calibrateArm() {
 	// Write arm values to serial to allow for manual calibration of presets
-	int value = analogRead(1);
+	int value = analogRead(ARMPOT);
 	printf("%u\n", value);
 }
 
@@ -41,8 +50,8 @@ void controlDrive() {
 	if (driveMode == DEFAULT_DRIVE) {
 		int left = joystickGetAnalog(1, 3);
 		int right = joystickGetAnalog(1, 2);
-		motorSet(2, left);
-		motorSet(3, -right);
+		motorSet(L_DRIVE, left);
+		motorSet(R_DRIVE, -right);
 	} else if (driveMode == CHANDLER_DRIVE) {
 		int steer = joystickGetAnalog(1, 4);
 		int speed = joystickGetAnalog(1, 2);
@@ -69,8 +78,8 @@ void controlDrive() {
 				right *= steer;
 			}
 		}
-		motorSet(2, left);
-		motorSet(3, right);
+		motorSet(L_DRIVE, left);
+		motorSet(R_DRIVE, right);
 	}
 }
 
@@ -78,11 +87,11 @@ void controlPunch() {
 	bool forward = joystickGetDigital(1, 6, JOY_UP);
 	bool reverse = joystickGetDigital(1, 6, JOY_DOWN);
 	if (forward) {
-		motorSet(1, 127);
+		motorSet(PUNCHER, 127);
 	} else if (reverse) {
-		motorSet(1, -128);
+		motorSet(PUNCHER, -128);
 	} else {
-		motorSet(1, 0);
+		motorSet(PUNCHER, 0);
 	}
 }
 
@@ -95,17 +104,18 @@ void controlArm() {
 	bool pos4 = joystickGetDigital(1, 8, JOY_UP);
 	//bool deploy = joystickGetDigital(1, 7 , JOY_LEFT);
 	//bool retract = joystickGetDigital(1, 7, JOY_RIGHT);
-	if (up) {
+	int currentArmState = analogRead(1);
+	if (up && currentArmState < ARM_MAX) {
 		armHolding = false;
-		motorSet(10, -16);
-	} else if (down) {
+		motorSet(ARM, -16);
+	} else if (down && currentArmState > ARM_MIN) {
 		armHolding = false;
-		motorSet(10, 16);
+		motorSet(ARM, 16);
 	} else {
 		if (!armHolding) {
 			armHolding = true;
-			motorSet(10, 0); // pause until maintainArm() can take over
-			armlevel = analogRead(1);
+			motorSet(ARM, 0); // pause until maintainArm() can take over
+			armlevel = analogRead(ARMPOT);
 		}
 	}
 	if (pos1) {
@@ -132,50 +142,53 @@ void controlIntake() {
 		}
 
 		if (reverse) {
-			motorSet(4, 127);
+			motorSet(INTAKE, 127);
 		} else {
 			if (intakeRunning) {
-				motorSet(4, -128);
+				motorSet(INTAKE, -128);
 			} else {
-				motorSet(4, 0);
+				motorSet(INTAKE, 0);
 			}
 		}
 	} else {
 	 // dumb stupid intake controls
 		if (reverse) {
-			motorSet(4, 127);
+			motorSet(INTAKE, 127);
 		} else if (forward) {
-			motorSet(4, -128);
+			motorSet(INTAKE, -128);
 		} else {
-			motorSet(4, 0);
+			motorSet(INTAKE, 0);
 		}
 	}
 }
 
 void maintainArm() {
-	if (!armHolding) {
-		return;
-	}
-	int currentArmState = analogRead(1);
-	int scalar;
-	int delta = abs(currentArmState - armlevel);
-	if (delta < 32) {
-		scalar = 4;
-	} else if (delta < 64) {
-		scalar = 8;
-	} else if (delta < 128) {
-		scalar = 16;
-	} else if (delta < 256) {
-		scalar = 32;
+	int currentArmState = analogRead(ARMPOT);
+	if (armHolding) {
+		int scalar;
+		int delta = abs(currentArmState - armlevel);
+		if (delta < 32) {
+			scalar = 4;
+		} else if (delta < 64) {
+			scalar = 8;
+		} else if (delta < 128) {
+			scalar = 16;
+		} else if (delta < 256) {
+			scalar = 32;
+		} else {
+			scalar = 64;
+		}
+		if (currentArmState < armlevel - ARM_EPSILON) {
+			motorSet(ARM, -scalar);
+		} else if (currentArmState > armlevel + ARM_EPSILON) {
+			motorSet(ARM, scalar);
+		} else {
+			motorSet(ARM, 0);
+		}
 	} else {
-		scalar = 64;
-	}
-	if (currentArmState < armlevel - ARM_EPSILON) {
-		motorSet(10, -scalar);
-	} else if (currentArmState > armlevel + ARM_EPSILON) {
-		motorSet(10, scalar);
-	} else {
-		motorSet(10, 0);
+		if (currentArmState > ARM_MAX || currentArmState < ARM_MIN) {
+			motorSet(ARM, 0);
+		}
 	}
 }
 
