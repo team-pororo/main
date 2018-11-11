2cddd371b52b9ca6a4c67e31c541c26a42ea0dca
Wesley Chalmers <wesleyjchalmers@gmail.com>
2018-11-11 11:16:24 -0500

    Added ChandlerDrive controls, an option for legacy intake controls, and better manual arm control.

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include "math.h"/*
-#include "drivetrain.h"
-#include "balllift.h"
-#include "claw.h"
-#include "clawlift.h"
-#include "launcher.h"
-#include "lifter.h"*/
+#include "math.h"
 
+// Team Pororo Robot Operator Control Code
+// Copyright (c) Wesley Chalmers, 2018
+
+
+// arm constants
 #define ARM_MIN 3200
 #define ARM_FLIP 2700
 #define ARM_UNDER 2110 // goes under an elevated cap, doesn't flip it
@@ -14,20 +13,65 @@
 #define ARM_MAX 1000
 #define ARM_EPSILON 16 // resolution
 
+// intake-forward button is toggle on/off so it doesn't have to be held down
+bool toggleIntake = true;
+
+// Drive mode
+#define DEFAULT_DRIVE 0 // Wesley's DefaultDrive control set - tank steer
+#define CHANDLER_DRIVE 1 // Chandler's steer/speed-drive control set
+int driveMode = DEFAULT_DRIVE;
+
+
+// flickturn constants:
+#define LEFT_FLICK -64
+#define RIGHT_FLICK 64
+
 int armlevel = ARM_MIN;
+bool armHolding = true; // fake-pid can be disabled for manual arm control
 bool intakeRunning = 0;
 bool intakeButtonPressed = 0;
 
 void calibrateArm() {
+	// Write arm values to serial to allow for manual calibration of presets
 	int value = analogRead(1);
 	printf("%u\n", value);
 }
 
 void controlDrive() {
-	int left = joystickGetAnalog(1, 3);
-	int right = -joystickGetAnalog(1, 2) * 0.8;
-	motorSet(2, left);
-	motorSet(3, right);
+	if (driveMode == DEFAULT_DRIVE) {
+		int left = joystickGetAnalog(1, 3);
+		int right = joystickGetAnalog(1, 2);
+		motorSet(2, left);
+		motorSet(3, -right);
+	} else if (driveMode == CHANDLER_DRIVE) {
+		int steer = joystickGetAnalog(1, 4);
+		int speed = joystickGetAnalog(1, 2);
+		// flick the right stick left or right for a quick 90deg turn
+		int flickturn = joystickGetAnalog(1, 1);
+
+		int left = 0;
+		int right = 0;
+
+		if (flickturn < LEFT_FLICK) {
+			left = -128;
+			right = 127;
+		} else if (flickturn > RIGHT_FLICK) {
+			left = 127;
+			right = -128;
+		} else {
+			left = speed;
+			right = speed;
+			if (steer < 0) {
+				steer = (128 - steer) / 128;
+				left *= -steer;
+			} else {
+				steer = (128 - steer) / 128;
+				right *= steer;
+			}
+		}
+		motorSet(2, left);
+		motorSet(3, right);
+	}
 }
 
 void controlPunch() {
@@ -52,9 +96,17 @@ void controlArm() {
 	//bool deploy = joystickGetDigital(1, 7 , JOY_LEFT);
 	//bool retract = joystickGetDigital(1, 7, JOY_RIGHT);
 	if (up) {
-		armlevel += 50;
+		armHolding = false;
+		motorSet(10, -16);
 	} else if (down) {
-		armlevel -= 50;
+		armHolding = false;
+		motorSet(10, 16);
+	} else {
+		if (!armHolding) {
+			armHolding = true;
+			motorSet(10, 0); // pause until maintainArm() can take over
+			armlevel = analogRead(1);
+		}
 	}
 	if (pos1) {
 		armlevel = ARM_MIN;
@@ -70,15 +122,29 @@ void controlArm() {
 void controlIntake() {
 	bool forward = joystickGetDigital(1, 5, JOY_UP);
 	bool reverse = joystickGetDigital(1, 5, JOY_DOWN);
-	if (forward != intakeButtonPressed) {
-		intakeButtonPressed = forward;
-		if (forward) {
-			intakeRunning = !intakeRunning;
+	if (toggleIntake) {
+		// watch for button presses, update running state
+		if (forward != intakeButtonPressed) {
+			intakeButtonPressed = forward;
+			if (forward) {
+				intakeRunning = !intakeRunning;
+			}
 		}
-	} else if (reverse) {
-		motorSet(4, 127);
+
+		if (reverse) {
+			motorSet(4, 127);
+		} else {
+			if (intakeRunning) {
+				motorSet(4, -128);
+			} else {
+				motorSet(4, 0);
+			}
+		}
 	} else {
-	  if (intakeRunning) {
+	 // dumb stupid intake controls
+		if (reverse) {
+			motorSet(4, 127);
+		} else if (forward) {
 			motorSet(4, -128);
 		} else {
 			motorSet(4, 0);
@@ -87,6 +153,9 @@ void controlIntake() {
 }
 
 void maintainArm() {
+	if (!armHolding) {
+		return;
+	}
 	int currentArmState = analogRead(1);
 	int scalar;
 	int delta = abs(currentArmState - armlevel);
@@ -116,7 +185,7 @@ void operatorControl() {
 		controlDrive();
 		controlIntake();
 		controlPunch();
-    calibrateArm();
+    //calibrateArm();
 		maintainArm();
 		controlArm();
 		delay(20);
