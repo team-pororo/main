9418cc4c72e9f8c5366950f3247941eeb1b00e5e
Wesley Chalmers <wesleyjchalmers@gmail.com>
2018-11-13 18:27:05 -0500

    Add Chandler's controls, adjust arm presets

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -8,18 +8,18 @@
 #define L_DRIVE 2
 #define R_DRIVE 3
 #define INTAKE 4
-#define PUNCHER 1
-#define ARM 10
+#define PUNCHER 5
+#define ARM 1
 
 // sensor ports
 #define ARMPOT 1
 
 // arm constants
-#define ARM_MIN 3200
-#define ARM_FLIP 2700
-#define ARM_UNDER 2110 // goes under an elevated cap, doesn't flip it
-#define ARM_DESCORE 1570
-#define ARM_MAX 1000
+#define ARM_MIN 3200 // minimum safe position before hitting ground
+#define ARM_FLIP 2700 // flip a cap on the ground
+#define ARM_UNDER 1900 // goes under an elevated cap, doesn't flip it
+#define ARM_DESCORE 1570 // max vertical position, descore a cap on post
+#define ARM_MAX 1000 // maximum safe position vertically before hitting intake
 #define ARM_EPSILON 16 // resolution
 
 // intake-forward button is toggle on/off so it doesn't have to be held down
@@ -28,12 +28,60 @@ bool toggleIntake = true;
 // Drive mode
 #define DEFAULT_DRIVE 0 // Wesley's DefaultDrive control set - tank steer
 #define CHANDLER_DRIVE 1 // Chandler's steer/speed-drive control set
-int driveMode = DEFAULT_DRIVE;
+												 // Like Team 254 "cheesy drive" or two-stick arcade drive
 
+bool inverseDriveButtonState = false;
+bool inverseDriving = false;
+bool driveModeButtonState = false;
+bool driveMode = DEFAULT_DRIVE;
 
-// flickturn constants:
-#define LEFT_FLICK -64
-#define RIGHT_FLICK 64
+void controlDrive() {
+	bool driveModeButton = joystickGetDigital(1, 7, JOY_UP);
+	if (driveModeButton != driveModeButtonState) {
+		driveModeButtonState = driveModeButton;
+		if (driveModeButton) {
+			printf("Toggling drive mode");
+			driveMode = !driveMode;
+		}
+	}
+	bool inverseDrive = joystickGetDigital(1, 7, JOY_RIGHT);
+	if (inverseDrive != inverseDriveButtonState) {
+		inverseDriveButtonState = inverseDrive;
+		if (inverseDrive) {
+			inverseDriving = !inverseDriving;
+		}
+	}
+	if (driveMode == DEFAULT_DRIVE) {
+		int left = joystickGetAnalog(1, 3);
+		int right = joystickGetAnalog(1, 2);
+		if (inverseDriving) {
+			motorSet(L_DRIVE, -right);
+			motorSet(R_DRIVE, left * 0.9);
+		} else {
+			motorSet(L_DRIVE, left);
+			motorSet(R_DRIVE, -right * 0.9);
+		}
+	} else if (driveMode == CHANDLER_DRIVE) {
+		int x = -joystickGetAnalog(1, 4);
+		int y = joystickGetAnalog(1, 2);
+		int l = 0;
+		int r = 0;
+
+		int v = (128-abs(x))*(y/128)+y;
+		int w = (128-abs(y))*(x/128)+x;
+
+		r = (v+w)/2;
+		l = (v-w)/2;
+
+		if (inverseDrive) {
+			motorSet(L_DRIVE, -r);
+			motorSet(R_DRIVE, l);
+		} else {
+			motorSet(L_DRIVE, l);
+			motorSet(R_DRIVE, -r);
+		}
+	}
+}
 
 int armlevel = ARM_MIN;
 bool armHolding = true; // fake-pid can be disabled for manual arm control
@@ -43,44 +91,7 @@ bool intakeButtonPressed = 0;
 void calibrateArm() {
 	// Write arm values to serial to allow for manual calibration of presets
 	int value = analogRead(ARMPOT);
-	printf("%u\n", value);
-}
-
-void controlDrive() {
-	if (driveMode == DEFAULT_DRIVE) {
-		int left = joystickGetAnalog(1, 3);
-		int right = joystickGetAnalog(1, 2);
-		motorSet(L_DRIVE, left);
-		motorSet(R_DRIVE, -right);
-	} else if (driveMode == CHANDLER_DRIVE) {
-		int steer = joystickGetAnalog(1, 4);
-		int speed = joystickGetAnalog(1, 2);
-		// flick the right stick left or right for a quick 90deg turn
-		int flickturn = joystickGetAnalog(1, 1);
-
-		int left = 0;
-		int right = 0;
-
-		if (flickturn < LEFT_FLICK) {
-			left = -128;
-			right = 127;
-		} else if (flickturn > RIGHT_FLICK) {
-			left = 127;
-			right = -128;
-		} else {
-			left = speed;
-			right = speed;
-			if (steer < 0) {
-				steer = (128 - steer) / 128;
-				left *= -steer;
-			} else {
-				steer = (128 - steer) / 128;
-				right *= steer;
-			}
-		}
-		motorSet(L_DRIVE, left);
-		motorSet(R_DRIVE, right);
-	}
+	printf("%u\t%u\n", value, armlevel);
 }
 
 void controlPunch() {
@@ -96,7 +107,7 @@ void controlPunch() {
 }
 
 void controlArm() {
-	bool up = joystickGetDigital(1, 7, JOY_RIGHT);
+	bool up = joystickGetDigital(1, 7, JOY_LEFT);
 	bool down = joystickGetDigital(1, 7, JOY_DOWN);
 	bool pos1 = joystickGetDigital(1, 8, JOY_DOWN);
 	bool pos2 = joystickGetDigital(1, 8, JOY_LEFT);
@@ -105,12 +116,15 @@ void controlArm() {
 	//bool deploy = joystickGetDigital(1, 7 , JOY_LEFT);
 	//bool retract = joystickGetDigital(1, 7, JOY_RIGHT);
 	int currentArmState = analogRead(1);
-	if (up && currentArmState < ARM_MAX) {
+	//printf("UP/DN:\t%u\t%u\t%u\n", up, down, currentArmState);
+	if (down && currentArmState < ARM_MIN) {
 		armHolding = false;
+		//printf("Setting armHolding to false");
 		motorSet(ARM, -16);
-	} else if (down && currentArmState > ARM_MIN) {
+	} else if (up && currentArmState > ARM_MAX) {
 		armHolding = false;
-		motorSet(ARM, 16);
+		//printf("Setting armHolding to false");
+		motorSet(ARM, 32);
 	} else {
 		if (!armHolding) {
 			armHolding = true;
@@ -166,6 +180,7 @@ void maintainArm() {
 
 	// WHO'S CALLING MY CODE SHADY! I PITY THE FOOL WHO'S CALLIN WESLEY'S
 	// CODE SHADY! PITY I SAY!
+
 	int currentArmState = analogRead(ARMPOT);
 	if (armHolding) {
 		int scalar;
@@ -189,7 +204,7 @@ void maintainArm() {
 			motorSet(ARM, 0);
 		}
 	} else {
-		if (currentArmState > ARM_MAX || currentArmState < ARM_MIN) {
+		if (currentArmState < ARM_MAX || currentArmState > ARM_MIN) {
 			motorSet(ARM, 0);
 		}
 	}
